66e39e573cd38a07c6295702787a92cf8b03a81d
Emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2023-08-14 18:25:23 +0100

    feat: create transaction inputs

--- src/services/wallet/transaction/index.js
+++ src/services/wallet/transaction/index.js
@@ -4,13 +4,26 @@ class Transaction {
   constructor({ senderWallet, recipient, amount }) {
     this.id = v1();
     this.outputMap = this.createOutputMap({ senderWallet, recipient, amount });
+    this.input = this.createInput({ senderWallet, outputMap: this.outputMap });
   }
 
   createOutputMap({ senderWallet, recipient, amount }) {
     const outputMap = {};
     outputMap[recipient] = amount;
     outputMap[senderWallet.publicKey] = senderWallet.balance - amount;
+    // console.log('output',outputMap);
     return outputMap;
+  }
+
+  createInput({ senderWallet, outputMap }) {
+    const input = {
+      timestamp: Date.now(),
+      amount: senderWallet.balance,
+      address: senderWallet.publicKey,
+      signature: senderWallet.sign(outputMap)
+    };
+    // console.log('input',input);
+    return input;
   }
 }
 
