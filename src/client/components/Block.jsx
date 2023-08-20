c290dffec5a9d019317ea82ac07cab97a5b05b68
Emmanueltoluwanimi <emmanueltoluwanimi01@gmail.com>
2023-08-20 02:01:19 +0100

    style: crypto ui

--- src/client/components/Block.jsx
+++ src/client/components/Block.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Block({block}) {
   const {hash, timestamp, data} = block;
   const hashDisplay = `${hash.substring(0, 15)}...`;
   const stringifiedData = JSON.stringify(data);
   const dataDisplay = stringifiedData.length > 35 ? `${stringifiedData.substring(0, 35)}...` : stringifiedData;
+
+  const [displayTransaction, setDisplayTransaction] = useState(false);
+  function ToggleTransaction() {
+    setDisplayTransaction(!displayTransaction);
+  }
+  
 
   return (
     <div className='Blocks'>
